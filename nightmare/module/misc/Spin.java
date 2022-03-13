06635505aabe9688d89c5c99fc00341095100566
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2022-03-13 22:57:02 +0900

    Add new Spin Mode(Left Right Random)

--- nightmare/module/misc/Spin.java
+++ nightmare/module/misc/Spin.java
@@ -1,5 +1,8 @@
 package nightmare.module.misc;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import net.minecraft.client.entity.EntityPlayerSP;
 import nightmare.Nightmare;
 import nightmare.event.EventTarget;
@@ -12,14 +15,45 @@ public class Spin extends Module{
 
     private float yaw;
     
+    private Random random = new Random();
+    
+    private int randomval;
 	public Spin() {
 		super("Spin", 0, Category.MISC);
 
+		ArrayList<String> options = new ArrayList<>();
+		
+		options.add("Left");
+		options.add("Right");
+		options.add("Random");
+		
 		Nightmare.instance.settingsManager.rSetting(new Setting("Speed", this, 55, 1, 100, false));
+		Nightmare.instance.settingsManager.rSetting(new Setting("Mode", this, "Left", options));
+	}
+	
+	@EventTarget
+	public void onTick(EventTick event) {
+		String mode = Nightmare.instance.settingsManager.getSettingByName(this, "Mode").getValString();
+		
+		if(mode.equals("Left")) {
+			this.spinLeft();
+		}else if(mode.equals("Right")) {
+			this.spinRight();
+		}else if(mode.equals("Random")) {
+			
+			if(randomval == 0) {
+				this.spinLeft();
+			}
+			
+			if(randomval == 1) {
+				this.spinRight();
+			}
+		}
 	}
 	
 	@Override
 	public void onEnable() {
+		randomval = random.nextInt(2);
 		if(mc.thePlayer != null & mc.theWorld != null) {
 	        this.yaw = mc.thePlayer.rotationYaw;
 		}
@@ -34,8 +68,7 @@ public class Spin extends Module{
 		super.onDisable();
 	}
 	
-	@EventTarget
-	public void onTick(EventTick event) {
+	public void spinLeft() {
         double left = (double)this.yaw + 360 - (double)mc.thePlayer.rotationYaw;
         EntityPlayerSP player;
         if (left < Nightmare.instance.settingsManager.getSettingByName(this, "Speed").getValDouble()) {
@@ -46,6 +79,22 @@ public class Spin extends Module{
             player = mc.thePlayer;
             player.rotationYaw = (float) ((double)player.rotationYaw + Nightmare.instance.settingsManager.getSettingByName(this, "Speed").getValDouble());
             if ((double)mc.thePlayer.rotationYaw >= (double)this.yaw + 360) {
+                this.setToggled(false);
+            }
+        }
+	}
+	
+	public void spinRight() {
+        double right = (double)this.yaw - 360 - (double)mc.thePlayer.rotationYaw;
+        EntityPlayerSP player;
+        if (right > -Nightmare.instance.settingsManager.getSettingByName(this, "Speed").getValDouble()) {
+            player = mc.thePlayer;
+            player.rotationYaw = (float)((double)player.rotationYaw + right);
+            this.setToggled(false);
+        } else {
+            player = mc.thePlayer;
+            player.rotationYaw = (float) ((double)player.rotationYaw - Nightmare.instance.settingsManager.getSettingByName(this, "Speed").getValDouble());
+            if ((double)mc.thePlayer.rotationYaw <= (double)this.yaw - 360) {
                 this.setToggled(false);
             }
         }
