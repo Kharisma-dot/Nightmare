e37699cafcf8f17f61fe41b02397814d46d2d07e
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2022-03-17 12:51:42 +0900

    Version1.1 Soon?

--- nightmare/Nightmare.java
+++ nightmare/Nightmare.java
@@ -23,7 +23,7 @@ public class Nightmare {
 	
 	public static Nightmare instance = new Nightmare();
 	
-	private String name = "Nightmare", version = "1.0";
+	private String name = "Nightmare", version = "1.1";
 	
     public SettingsManager settingsManager;
     public EventManager eventManager;
